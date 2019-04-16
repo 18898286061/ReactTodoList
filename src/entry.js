0189a51874a682ea73335e9e9f1a4fddb41d4a6a
KaiKai <1035759181@qq.com>
2019-04-16 17:22:29 +0800

    initTodo

---
+++ src/entry.js
@@ -0,0 +1,5 @@
+'use strict';
+// require('./components/app')
+// require('./styles/main.styl')
+import './components/app'  //es6写法
+import './styles/main.styl'   //es6写法 
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/entry.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(styl|css)$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'stylus-loader' }],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ]
+  }
+};
